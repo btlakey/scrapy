405951e8154d06298dba01d337c26d4e18f8765d
Brian Lakey <brianlakey@gmaile.com>
2023-02-17 13:41:09 -0600

    building bookshelf payload
    	new file:   ../.gitignore
    	modified:   goodreads/spiders/goodreads_spider.py

--- goodreads/goodreads/spiders/goodreads_spider.py
+++ goodreads/goodreads/spiders/goodreads_spider.py
@@ -1,5 +1,5 @@
-
 import scrapy
+from toolz import curry
 
 
 class ShelvesSpider(scrapy.Spider):
@@ -11,11 +11,59 @@ class ShelvesSpider(scrapy.Spider):
         "https://www.goodreads.com/review/list/40648422"
     ]
 
+    def response_get(self, response, x, is_int=False):
+        val = response.xpath(x).get()
+        if is_int:
+            int(str.strip(val))
+        return val
+
+    def convert_rating(self, rating):
+
+
     def parse(self, response):
         shelf_urls = response.xpath(
                 f"//*[@id='paginatedShelfList']//*[re:test(@title, '{self.shelf_names_re}')]/@href"
             )
-        yield from response.follow_all(shelf_urls, self.parse_shelves)
+        yield from response.follow_all(shelf_urls, self.parse_shelf)
 
-    def parse_shelves(self, response):
-        print(response.xpath("//*[@id='booksBody']").get())
+    def parse_shelf(self, response_shelf):
+        for book in response_shelf.xpath(
+                "//*[@id='booksBody']//*[@class='bookalike review']"
+        ):
+            yield self.parse_book(book)
+
+    def parse_book(self, response_book):
+        # book_xpath = lambda x: response_book.xpath(x).get()
+        book_xpath = curry(self.response_get)(response_book)
+
+        yield {
+            "title": book_xpath(
+                "//*[@class='field title']//@title"
+            ),
+            "isbn13": book_xpath(
+                "//*[@class='field isbn13']//div//text()", is_int=True
+            ),
+            "author": book_xpath(
+                "//*[@class='field author']//a//text()"
+            ),
+            "date_pub": book_xpath(
+                "//*[@class='field date_pub']//div//text()", is_int=True
+            )
+            "mean_rating": book_xpath(
+                "//*[@class='field avg_rating']//div//text()", is_int=True
+            ),
+            "num_rating": book_xpath(
+                "//*[@class='field num_ratings']//div//text()", is_int=True
+            ),
+            "user_rating": book_xpath(
+                , is_int=True
+            ),
+            "date_read": book_xpath(),
+            "date_added": book_xpath(),
+            "review_text": self.parse_review(review)
+        }
+
+
+
+
+
